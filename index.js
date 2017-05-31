61f256bbfdc86426d93083e8a91c2608ba392833
pkindalov <panayotkindalov@gmail.com>
2017-05-31 13:29:51 +0300

    Writed handler metod with request params

--- index.js
+++ index.js
@@ -21,7 +21,14 @@ app.get('/category/:name', (req, res) => {
     console.log(req.params.name);
     res.send('Category name is ' + req.params.name);
     res.end();
+});
 
+app.get('/users/:userId/books/:bookId', (req, res) => {
+    console.log(req.params);
+    console.log(req.params.userId);
+    console.log(req.params.bookId);
+    res.send(`User Id: ${req.params.userId} Book id: ${req.params.bookId}`);
+    res.end();
 });
 
 
