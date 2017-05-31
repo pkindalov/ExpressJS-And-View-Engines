b4fc3b1c5e4c8052c8553e9aa788f52ffe129519
pkindalov <panayotkindalov@gmail.com>
2017-05-31 13:20:20 +0300

    Added different handler methods

--- index.js
+++ index.js
@@ -6,6 +6,29 @@ app.get('/', (req, res) => {
     res.send('Hello, it\'s me');
 });
 
+app.post('/post', (req, res) => {
+    res.send('It\`s posted.');
+});
+
+
+app.get(/.*fly$/, (req, res) => {
+    res.send('butterfly, dragonfly');
+});
+
+
+app.get('/category/:name', (req, res) => {
+    console.log(req.params);
+    console.log(req.params.name);
+    res.send('Category name is ' + req.params.name);
+    res.end();
+
+});
+
+
+app.all('*', (req, res) => {
+    res.send('Ooops 404');
+});
+
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
