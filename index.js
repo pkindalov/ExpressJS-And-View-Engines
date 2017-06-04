a44b9a42560af93aa628d00bc56e07ec3a494de9
pkindalov <panayotkindalov@gmail.com>
2017-06-04 14:40:54 +0300

    Config view enging - pug in index.js

--- index.js
+++ index.js
@@ -4,53 +4,59 @@ const port = 3000;
 let app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
+app.set('views', 'public/views');
 
 app.use('/public', express.static('./public'));
+
+app.get('/', (req, res) => {
+    res.render('test', {title: 'Title from backend'});
+});
+
 
 // app.get('/', (req, res) => {
 //     res.send('Hello, it\'s me');
 // });
 
 //Middleware
-app.get('/', (req, res, next) =>{
-    req.user = {username: 'Banana Joe'};
-    next();
-}, (req, res) => {
-    res.send(req.user.username);
-    res.end();
-});
+// app.get('/', (req, res, next) =>{
+//     req.user = {username: 'Banana Joe'};
+//     next();
+// }, (req, res) => {
+//     res.send(req.user.username);
+//     res.end();
+// });
 
 
-app.post('/user/login', (req, res) => {
-   console.log(req.body);
-});
+// app.post('/user/login', (req, res) => {
+//    console.log(req.body);
+// });
 
 
-app.post('/post', (req, res) => {
-    res.send('It\`s posted.');
-});
+// app.post('/post', (req, res) => {
+//     res.send('It\`s posted.');
+// });
 
 
-app.get(/.*fly$/, (req, res) => {
-    res.send('butterfly, dragonfly');
-});
+// app.get(/.*fly$/, (req, res) => {
+//     res.send('butterfly, dragonfly');
+// });
 
 
-app.get('/category/:name', (req, res) => {
-    console.log(req.params);
-    console.log(req.params.name);
-    res.send('Category name is ' + req.params.name);
-    res.end();
-});
+// app.get('/category/:name', (req, res) => {
+//     console.log(req.params);
+//     console.log(req.params.name);
+//     res.send('Category name is ' + req.params.name);
+//     res.end();
+// });
 
-app.get('/users/:userId/books/:bookId', (req, res) => {
-    console.log(req.params);
-    console.log(req.params.userId);
-    console.log(req.params.bookId);
-    res.send(`User Id: ${req.params.userId} Book id: ${req.params.bookId}`);
-    res.end();
-});
+// app.get('/users/:userId/books/:bookId', (req, res) => {
+//     console.log(req.params);
+//     console.log(req.params.userId);
+//     console.log(req.params.bookId);
+//     res.send(`User Id: ${req.params.userId} Book id: ${req.params.bookId}`);
+//     res.end();
+// });
 
 
 app.all('*', (req, res) => {
